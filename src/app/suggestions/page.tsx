b8bfdceb007d57715a1062065eb272afddae4bb0
Eboh4119 <favourbenjamin2018@gmail.com>
2024-05-11 14:38:32 -0700

    Fix upcoming

--- src/app/suggestions/page.tsx
+++ src/app/suggestions/page.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+"use client";
+import React, {useState} from "react";
+import Image from "next/image";
 
 const page = () => {
-  return <div>suggestions</div>;
-};
+  const [userPic, setUserPic] = useState<File | null>(null);
+
+  function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+    const files = e.target.files;
+    if (files) setUserPic(files[0]);
+  }
+  return (
+    <div className=" ml-14 relative w-fit">
+    {/* Here */}
+    {userPic ? (
+      <Image
+        src={URL.createObjectURL(userPic)}
+        width={105}
+        height={106}
+        alt=""
+        className="rounded"
+      />
+    ) : (
+      <Image
+        src="/accDummy.svg"
+        width={105}
+        height={106}
+        alt=""
+        className="rounded"
+      />
+    )}
+
+
+                    <div className="w-fit relative">
+                      
+                      <input
+                        type="file"
+                        accept=".png, .jpeg, .jpg"
+                        className="absolute top-0 -left-3"
+                        onChange={(e) => handleInput(e)}
+                      />
+                    </div>
+     </div>               
+  )
+
+    
+}
 
 export default page;
